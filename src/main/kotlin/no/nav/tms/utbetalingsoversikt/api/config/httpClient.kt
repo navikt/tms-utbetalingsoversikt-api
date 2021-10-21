91acc488bf5d974585d2ac30d1e18569478bb1f7
Christopher Santana <christopher.santana@nav.no>
2021-10-21 11:11:11 +0200

    Set content-header.

--- src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/config/httpClient.kt
+++ src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/config/httpClient.kt
@@ -2,8 +2,7 @@ package no.nav.tms.utbetalingsoversikt.api.config
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.*
-import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpMethod
+import io.ktor.http.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.net.URL
@@ -11,6 +10,7 @@ import java.net.URL
 suspend inline fun <reified T> HttpClient.post(url: URL, requestBody: Any, token: String): T = withContext(Dispatchers.IO) {
     request {
         url(url)
+        contentType(ContentType.Application.Json)
         method = HttpMethod.Post
         body = requestBody
         header(HttpHeaders.Authorization, "Bearer $token")
