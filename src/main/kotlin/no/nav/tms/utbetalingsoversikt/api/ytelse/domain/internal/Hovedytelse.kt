2b292d1ca64d10210ba2cb34f11047f7510c0dd7
Christopher Santana <christopher.santana@nav.no>
2021-10-22 13:38:23 +0200

    Serialname.

--- src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/ytelse/domain/internal/Hovedytelse.kt
+++ src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/ytelse/domain/internal/Hovedytelse.kt
@@ -1,5 +1,6 @@
 package no.nav.tms.utbetalingsoversikt.api.ytelse.domain.internal
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import no.nav.tms.utbetalingsoversikt.api.config.LocalDateSerializer
 import java.time.LocalDate
@@ -9,14 +10,14 @@ data class Hovedytelse(
     val id: Int,
     val ytelse: String,
     val status: String,
-    @Serializable(with = LocalDateSerializer::class) val ytelseDato: LocalDate?,
-    @Serializable(with = LocalDateSerializer::class) val forfallDato: LocalDate?,
-    val ytelsePeriode: Periode?,
-    val utbetaltTil: String?,
+    @SerialName("ytelse_dato") @Serializable(with = LocalDateSerializer::class) val ytelseDato: LocalDate?,
+    @SerialName("forfall_dato") @Serializable(with = LocalDateSerializer::class) val forfallDato: LocalDate?,
+    @SerialName("ytelse_periode") val ytelsePeriode: Periode?,
+    @SerialName("utbetalt_til") val utbetaltTil: String?,
     val kontonummer: String,
     val underytelser: List<Underytelse>,
     val trekk: List<Trekk>,
-    val erUtbetalt: Boolean,
+    @SerialName("er_utbetalt") val erUtbetalt: Boolean,
     val rettighetshaver: Rettighetshaver,
     val melding: String
 )
--- src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/ytelse/domain/internal/Rettighetshaver.kt
+++ src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/ytelse/domain/internal/Rettighetshaver.kt
@@ -1,9 +1,10 @@
 package no.nav.tms.utbetalingsoversikt.api.ytelse.domain.internal
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Rettighetshaver (
-    val aktoerId: String,
+    @SerialName("aktoer_id") val aktoerId: String,
     val navn: String?
 )
