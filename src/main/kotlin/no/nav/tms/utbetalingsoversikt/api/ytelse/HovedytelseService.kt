fe7db719192cfff85c6059f0bd96e5635dffb3df
Christopher Santana <christopher.santana@nav.no>
2021-10-22 11:17:33 +0200

    Logger hver ytelse.

--- src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/ytelse/HovedytelseService.kt
+++ src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/ytelse/HovedytelseService.kt
@@ -2,9 +2,12 @@ package no.nav.tms.utbetalingsoversikt.api.ytelse
 
 import no.nav.tms.utbetalingsoversikt.api.ytelse.domain.transformer.HovedytelseTransformer
 import no.nav.tms.utbetalingsoversikt.api.ytelse.domain.internal.Hovedytelse
+import org.slf4j.LoggerFactory
 import java.time.LocalDate
 
 class HovedytelseService(private val consumer: SokosUtbetalingConsumer) {
+    val log = LoggerFactory.getLogger(HovedytelseService::class.java)
+
     suspend fun getHovedytelserBetaltTilBruker(ident: String, fom: LocalDate, tom: LocalDate): List<Hovedytelse> {
         return consumer.fetchUtbetalingsInfo(ident, fom, tom)
             .map(HovedytelseTransformer::toHovedYtelse)
@@ -13,6 +16,7 @@ class HovedytelseService(private val consumer: SokosUtbetalingConsumer) {
     }
 
     private fun Hovedytelse.isUtbetaltTilBruker(): Boolean {
+        log.info("$this")
         return if (utbetaltTil == null || rettighetshaver.navn == null) {
             false
         } else {
