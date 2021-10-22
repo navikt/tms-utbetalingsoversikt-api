b32ccce0cd1d196ec7e526675c133667b4abae68
Christopher Santana <christopher.santana@nav.no>
2021-10-22 13:38:23 +0200

    Bruk Serialname og sett feltnavn til snake case.

--- src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/ytelse/domain/internal/Rettighetshaver.kt
+++ src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/ytelse/domain/internal/Rettighetshaver.kt
@@ -1,5 +1,6 @@
 package no.nav.tms.utbetalingsoversikt.api.ytelse.domain.internal
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
--- src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/ytelse/domain/internal/Trekk.kt
+++ src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/ytelse/domain/internal/Trekk.kt
@@ -1,5 +1,6 @@
 package no.nav.tms.utbetalingsoversikt.api.ytelse.domain.internal
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import no.nav.tms.utbetalingsoversikt.api.config.BigDecimalSerializer
 import java.math.BigDecimal
@@ -7,6 +8,6 @@ import java.math.BigDecimal
 @Serializable
 data class Trekk (
     val id: Int,
-    val trekkType: String,
-    @Serializable(with = BigDecimalSerializer::class) val trekkBelop: BigDecimal
+    @SerialName("trekk_type") val trekkType: String,
+    @SerialName("trekk_belop") @Serializable(with = BigDecimalSerializer::class) val trekkBelop: BigDecimal
 )
