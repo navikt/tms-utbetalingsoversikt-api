88c97ccd07b5a44926579167452400927f847464
Christopher Santana <christopher.santana@nav.no>
2021-11-19 11:18:23 +0100

    Revert "Legger på logging av hovedytelsefiltrering for å se om dette er nødvendig i det hele tatt."
    This reverts commit a4a81bf887f1c57fcd2503752bee2f0346aacfbc.

--- src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/utbetaling/UtbetalingService.kt
+++ src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/utbetaling/UtbetalingService.kt
@@ -4,12 +4,9 @@ import no.nav.tms.token.support.idporten.user.IdportenUser
 import no.nav.tms.utbetalingsoversikt.api.ytelse.HovedytelseService
 import no.nav.tms.utbetalingsoversikt.api.ytelse.HovedytelseComparator
 import no.nav.tms.utbetalingsoversikt.api.ytelse.domain.internal.Hovedytelse
-import org.slf4j.LoggerFactory
 import java.time.LocalDate
 
 class UtbetalingService(private val hovedytelseService: HovedytelseService) {
-
-    private val log = LoggerFactory.getLogger(UtbetalingService::class.java)
 
     suspend fun fetchUtbetalingForPeriod(user: IdportenUser, fromDateString: String?, toDateString: String?): UtbetalingResponse {
 
@@ -17,23 +14,13 @@ class UtbetalingService(private val hovedytelseService: HovedytelseService) {
         val adjustedFromDate = InputDateService.getEarlierFromDateWithinMaxBound(fromDate)
         val toDate = InputDateService.getToDate(toDateString)
 
-        val unfilteredHovedYtelser = hovedytelseService.getHovedytelserBetaltTilBruker(user, adjustedFromDate, toDate)
-
-        val filteredAndSortedHovedytelser = unfilteredHovedYtelser.filter { it.isWithinPeriod(fromDate, toDate) }
+        return hovedytelseService.getHovedytelserBetaltTilBruker(user, adjustedFromDate, toDate)
+            .filter { it.isInPeriod(fromDate, toDate)}
             .sortedWith(HovedytelseComparator::compareYtelse)
-
-        conditionallyLogFilteringResult(unfilteredHovedYtelser, filteredAndSortedHovedytelser)
-
-        return createUtbetalingResponse(filteredAndSortedHovedytelser)
+            .let { createUtbetalingResponse(it) }
     }
 
-    private fun conditionallyLogFilteringResult(unfiltered: List<Hovedytelse>, filtered: List<Hovedytelse>) {
-        if (unfiltered.size != filtered.size) {
-            log.info("Filtrerte bort ${unfiltered.size - filtered.size} hovedytelser som lå utenfor ønsket tidsrom.")
-        }
-    }
-
-    private fun Hovedytelse.isWithinPeriod(fromDate: LocalDate, toDate: LocalDate): Boolean {
+    private fun Hovedytelse.isInPeriod(fromDate: LocalDate, toDate: LocalDate): Boolean {
         return when {
             ytelseDato != null && erUtbetalt -> ytelseDato in fromDate..toDate
             ytelseDato == null -> false
