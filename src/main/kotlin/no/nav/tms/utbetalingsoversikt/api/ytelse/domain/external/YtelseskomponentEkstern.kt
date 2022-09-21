2907dfbea1c9a66403f8b0228dfd1cb05da1bc0b
Christopher Santana <christopher.santana@nav.no>
2022-09-21 11:37:15 +0200

    Logging.

--- src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/ytelse/domain/external/YtelseskomponentEkstern.kt
+++ src/main/kotlin/no/nav/tms/utbetalingsoversikt/api/ytelse/domain/external/YtelseskomponentEkstern.kt
@@ -1,6 +1,7 @@
 package no.nav.tms.utbetalingsoversikt.api.ytelse.domain.external
 
 import kotlinx.serialization.Serializable
+import no.nav.personbruker.dittnav.common.logging.util.logger
 
 @Serializable
 data class YtelseskomponentEkstern(
@@ -9,4 +10,8 @@ data class YtelseskomponentEkstern(
     val satstype: String? = null,
     val satsantall: Double? = null,
     val ytelseskomponentbeloep: Double? = null,
-)
+) {
+    init {
+        logger.info("Beløp: $satsbeloep, antall: $satsantall")
+    }
+}
